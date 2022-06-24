2f9544193fbf4c071c0e61f580f7f958ade6dab0
Carlos Chavarria <chavarriacarlos18@gmail.com>
2022-06-24 06:35:12 -0700

    global header

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Header() {
+    return (
+        <>
+            <Head>
+                <title>FireByte</title>
+                <meta name="description" content="Health, Code, and Life" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </>
+    );
+}
