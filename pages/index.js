6bca060b4b9bd5a4238aba12a41e38d4992ce791
Carlos Chavarria <chavarriacarlos18@gmail.com>
2022-06-25 19:03:14 -0700

    temp navbar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Header from '../components/Header'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Navbar />
+      
       <h1 className="text-3xl font-bold underline">
         Hello World!
       </h1>
