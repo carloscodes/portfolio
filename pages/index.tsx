5397bf4b7dc2217683a0d2576927ff3e640b4289
Carlos Chavarria <chavarriacarlos18@gmail.com>
2022-06-29 16:50:25 -0700

    color?

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
+import { Container } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <>
       <Header />
       <Navbar />
-      <h1>Hello</h1>
+      <Container className='text-center'>
+        <h1>Hello world!</h1>
+      </Container>
+      
 
       
     </>
