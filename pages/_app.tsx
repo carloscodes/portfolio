3dfac51421c9b6249dde50868c87411e5586270e
Carlos Chavarria <chavarriacarlos18@gmail.com>
2022-06-29 18:37:11 -0700

    dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,20 @@ import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+// 1. Import `extendTheme`
+import { extendTheme } from "@chakra-ui/react"
+
+// 2. Call `extendTheme` and pass your custom values
+const theme = extendTheme({
+  styles: {
+    global: '#000'
+  }
+})
 
 function MyApp({ Component, pageProps }: AppProps ) {
   return (
     <ThemeProvider>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
     </ThemeProvider>
