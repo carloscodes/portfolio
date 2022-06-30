c2ac368ee822c619be3bb8e9bb4798a52febe365
Carlos Chavarria <chavarriacarlos18@gmail.com>
2022-06-30 14:17:53 -0700

    apostrophe error

--- components/Hero.js
+++ components/Hero.js
@@ -13,7 +13,7 @@ export default function() {
                 <Heading as='h1' size='xl' noOfLines={2} className="text-gray-400 mb-4">
                     I build software for the web and iOS
                 </Heading>
-                <Text fontSize='xs' className="text-gray-400 mb-12">I'm a software engineer specializing in building (and occasionally) exceptional digital experiences. Currently, I'm focused on building an accessible, human-centered product to get people healthy and thriving. </Text>
+                <Text fontSize='xs' className="text-gray-400 mb-12">I&apos;m a software engineer specializing in building (and occasionally) exceptional digital experiences. Currently, I&apos;m focused on building an accessible, human-centered product to get people healthy and thriving. </Text>
                 <button className="bg-transparent text-green-400 font-light text-xs py-2 px-4 border border-green-400 rounded">
                     Check out my work!
                 </button>
