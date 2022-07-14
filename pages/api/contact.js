166a5b5d1fc2d3be02d04d689e4d1e9ef2ba9567
Carlos Chavarria <chavarriacarlos18@gmail.com>
2022-07-14 14:17:05 -0700

    await promise https://stackoverflow.com/questions/65631481/nodemailer-in-vercel-not-sending-email-in-production

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import nodemailer from 'nodemailer';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const {name, email, content } = req.body;
 
   const transporter = nodemailer.createTransport({
@@ -21,10 +21,19 @@ export default function handler(req, res) {
     subject: 'New Contact from Blog',
     text: `Name: ${name}\nEmail: ${email}\n\nMessage: ${content}`
   };
-  
-  const response = transporter.sendMail(mailOptions);
 
-  console.log(response);
+  await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.error(err);
+            reject(err);  
+        } else {
+            console.log(info);
+            resolve(info);
+        }
+    });
+  }); 
 
   res.status(200).json(req.body)
 }
