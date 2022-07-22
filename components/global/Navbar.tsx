84b6ea40cb89f113a625c9438dbb5182d5c28e3f
Carlos Chavarria <chavarriacarlos18@gmail.com>
2022-07-22 07:27:23 -0700

    fixed navbar

--- components/global/Navbar.tsx
+++ components/global/Navbar.tsx
@@ -1,5 +1,5 @@
 import { HamburgerIcon, SmallCloseIcon } from '@chakra-ui/icons';
-import { Button, CloseButton, Divider } from '@chakra-ui/react';
+import { Button, CloseButton, Divider, Drawer, DrawerOverlay, DrawerContent, DrawerBody, DrawerCloseButton, DrawerHeader, useDisclosure } from '@chakra-ui/react';
 import { useState } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
@@ -23,17 +23,22 @@ const variantsIcon = {
   }
 
 export default function Navbar() {
-    const [ isOpen, setIsOpen ] = useState(false)
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [ isNavOpen, setisNavOpen ] = useState(false)
+
+    function onClosed(){
+        toggleMenu()
+    }
 
     function toggleMenu(){
         let m = document.getElementById('mobile-menu')
 
         if(m!.classList.contains('hidden')){
-            setIsOpen(true)
+            setisNavOpen(true)
             m!.classList.remove('hidden')
             
         } else {
-            setIsOpen(false)
+            setisNavOpen(false)
             m!.classList.add('hidden')
         }
         
@@ -43,58 +48,109 @@ export default function Navbar() {
         <div className='mb-4 mt-2 text-slate-50 lg:px-12'>
             <nav className="flex items-center justify-between flex-wrap p-6">
                 <div className='lg:hidden'>
-                    <motion.div  animate={isOpen ? "closed" : "open"}
+                    <motion.div  animate={isNavOpen ? "closed" : "open"}
                     variants={variantsIcon} transition={{ duration: .5 }} className="block lg:hidden">
-                        {isOpen ? <CloseButton onClick={() => toggleMenu()} /> : <button className="flex items-center" onClick={() => toggleMenu()}>
+                        {isNavOpen ? <CloseButton onClick={() => toggleMenu()} /> : <button className="flex items-center" onClick={() => toggleMenu()}>
                              <HamburgerIcon />
                         </button> }
                     </motion.div>
                 </div>
-                
 
-                <motion.div animate={isOpen ? "open" : "closed"}
-                    variants={variants} transition={{ type: 'tween', duration: .5 }}  className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden" id="mobile-menu">
-                    <div className="text-sm lg:flex-grow">
-                    <Link href='/'>
+                <div className="text-sm lg:flex-grow hidden lg:block">
+                        <Link href='/'>
+                            <a className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0 mr-4 md:hover:bg-gray-900 hover:cursor-pointer rounded-md">
+                                Home
+                                {isOpen ? <div className='mt-4'><Divider /></div> : <div> </div>}
+                            </a>
+                            
+                        </Link>
+                        
+                        <Link href='/about'>
+                            <a className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0 mr-4 md:hover:bg-gray-900 hover:cursor-pointer rounded-md">
+                                About
+                                {isOpen ? <div className='mt-4'><Divider /></div> : <div> </div>}
+                            </a>
+                        </Link>
+                        <Link href='/work'>
+                            <a className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0 mr-4 md:hover:bg-gray-900 hover:cursor-pointer rounded-md">
+                                Work
+                                {isOpen ? <div className='mt-4'><Divider /></div> : <div> </div>}
+                            </a>
+                        </Link>
+                        <Link href='/blog'>
+                            <a className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0 mr-4 md:hover:bg-gray-900 hover:cursor-pointer rounded-md">
+                                Blog
+                                {isOpen ? <div className='mt-4'><Divider /></div> : <div> </div>}
+                            </a>
+                        </Link>
+                        
+
                         <a className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0 mr-4 md:hover:bg-gray-900 hover:cursor-pointer rounded-md">
-                            Home
+                            <Contact />
+                            {isOpen ? <div className='mt-4'><Divider /></div> : <div> </div>}
+                        </a>
+                        </div>
+                        <div className='mt-4 hidden lg:block'>
+                            <Link href='https://apps.apple.com/us/app/training-log/id1621350376'>
+                                <Button fontWeight="hairline" size="sm" variant="outline" color="blue.500" >Download App</Button>
+                            </Link>
+                        </div>
+            
+                        
+
+                <div className="hidden" id="mobile-menu">
+                    <Drawer placement='left' onClose={onClosed} isOpen={isNavOpen} size="full">
+                        <DrawerOverlay />
+                        <DrawerContent>
+                        <DrawerCloseButton />
+                        <DrawerHeader></DrawerHeader>
+                        <DrawerBody>
+                        <div className="text-sm lg:flex-grow">
+                        <Link href='/'>
+                            <a className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0 mr-4 md:hover:bg-gray-900 hover:cursor-pointer rounded-md">
+                                Home
+                                {isOpen ? <div className='mt-4'><Divider /></div> : <div> </div>}
+                            </a>
+                            
+                        </Link>
+                        
+                        <Link href='/about'>
+                            <a className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0 mr-4 md:hover:bg-gray-900 hover:cursor-pointer rounded-md">
+                                About
+                                {isOpen ? <div className='mt-4'><Divider /></div> : <div> </div>}
+                            </a>
+                        </Link>
+                        <Link href='/work'>
+                            <a className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0 mr-4 md:hover:bg-gray-900 hover:cursor-pointer rounded-md">
+                                Work
+                                {isOpen ? <div className='mt-4'><Divider /></div> : <div> </div>}
+                            </a>
+                        </Link>
+                        <Link href='/blog'>
+                            <a className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0 mr-4 md:hover:bg-gray-900 hover:cursor-pointer rounded-md">
+                                Blog
+                                {isOpen ? <div className='mt-4'><Divider /></div> : <div> </div>}
+                            </a>
+                        </Link>
+                        
+
+                        <a className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0 mr-4 md:hover:bg-gray-900 hover:cursor-pointer rounded-md">
+                            <Contact />
                             {isOpen ? <div className='mt-4'><Divider /></div> : <div> </div>}
                         </a>
                         
-                    </Link>
-                    
-                    <Link href='/about'>
-                        <a className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0 mr-4 md:hover:bg-gray-900 hover:cursor-pointer rounded-md">
-                            About
-                            {isOpen ? <div className='mt-4'><Divider /></div> : <div> </div>}
-                        </a>
-                    </Link>
-                    <Link href='/work'>
-                        <a className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0 mr-4 md:hover:bg-gray-900 hover:cursor-pointer rounded-md">
-                            Work
-                            {isOpen ? <div className='mt-4'><Divider /></div> : <div> </div>}
-                        </a>
-                    </Link>
-                    <Link href='/blog'>
-                        <a className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0 mr-4 md:hover:bg-gray-900 hover:cursor-pointer rounded-md">
-                            Blog
-                            {isOpen ? <div className='mt-4'><Divider /></div> : <div> </div>}
-                        </a>
-                    </Link>
-                    
-
-                    <a className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0 mr-4 md:hover:bg-gray-900 hover:cursor-pointer rounded-md">
-                        <Contact />
-                        {isOpen ? <div className='mt-4'><Divider /></div> : <div> </div>}
-                    </a>
-                    
-                    </div>
-                    <div className='mt-4'>
-                        <Link href='https://apps.apple.com/us/app/training-log/id1621350376'>
-                            <Button fontWeight="hairline" size="sm" variant="outline" color="blue.500" >Download App</Button>
-                        </Link>
-                    </div>
-                </motion.div>
+                        </div>
+                        <div className='mt-4'>
+                            <Link href='https://apps.apple.com/us/app/training-log/id1621350376'>
+                                <Button fontWeight="hairline" size="sm" variant="outline" color="blue.500" >Download App</Button>
+                            </Link>
+                        </div>
+              
+                        </DrawerBody>
+                        </DrawerContent>
+                    </Drawer>
+                </div>
+            
                 
             </nav>
         </div>
